9abcb01c9b8e3805d470aca66c953609221c61ab
Arlena Awal <arlenawalchaity@gmail.com>
2021-05-02 01:17:38 +0600

    add and show in grid view

---
+++ db_GUI sol/db_GUI/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace db_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbCName = new System.Windows.Forms.TextBox();
+            this.tbCCode = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.dtCourses = new System.Windows.Forms.DataGridView();
+            this.LoadData = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtCourses)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(63, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Course Name";
+            // 
+            // tbCName
+            // 
+            this.tbCName.Font = new System.Drawing.Font("Cambria", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCName.Location = new System.Drawing.Point(256, 57);
+            this.tbCName.Name = "tbCName";
+            this.tbCName.Size = new System.Drawing.Size(414, 43);
+            this.tbCName.TabIndex = 1;
+            // 
+            // tbCCode
+            // 
+            this.tbCCode.Font = new System.Drawing.Font("Cambria", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCCode.Location = new System.Drawing.Point(256, 145);
+            this.tbCCode.Name = "tbCCode";
+            this.tbCCode.Size = new System.Drawing.Size(414, 43);
+            this.tbCCode.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(63, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 36);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Course Code";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Cambria", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(540, 231);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(130, 60);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // dtCourses
+            // 
+            this.dtCourses.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtCourses.Location = new System.Drawing.Point(749, 57);
+            this.dtCourses.Name = "dtCourses";
+            this.dtCourses.RowTemplate.Height = 24;
+            this.dtCourses.Size = new System.Drawing.Size(505, 492);
+            this.dtCourses.TabIndex = 5;
+            // 
+            // LoadData
+            // 
+            this.LoadData.Font = new System.Drawing.Font("Cambria", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoadData.Location = new System.Drawing.Point(256, 231);
+            this.LoadData.Name = "LoadData";
+            this.LoadData.Size = new System.Drawing.Size(130, 60);
+            this.LoadData.TabIndex = 6;
+            this.LoadData.Text = "Load";
+            this.LoadData.UseVisualStyleBackColor = true;
+            this.LoadData.Click += new System.EventHandler(this.LoadData_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1266, 590);
+            this.Controls.Add(this.LoadData);
+            this.Controls.Add(this.dtCourses);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.tbCCode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbCName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtCourses)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbCName;
+        private System.Windows.Forms.TextBox tbCCode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DataGridView dtCourses;
+        private System.Windows.Forms.Button LoadData;
+    }
+}
+
---
+++ db_GUI sol/db_GUI/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Collections;
+
+namespace db_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            //do validation
+            string cname = tbCName.Text;
+            string ccode = tbCCode.Text;
+            string connString = @"Server=DESKTOP-VCKBA6J\SQLEXPRESS;Database=demo_m_sp21;Integrated Security=True;";
+            SqlConnection conn = new SqlConnection(connString);
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception ex) { 
+                MessageBox.Show(ex.Message);
+            }
+            string query = string. Format("insert into courses values ('{0}','{1}')",cname,ccode);
+            try
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                int r = cmd.ExecuteNonQuery();
+                if (r > 0)
+                {
+                    MessageBox.Show("Course Inserted");
+                }
+                else
+                {
+                    MessageBox.Show("Failed to Insert Course");
+                }
+            }
+            catch (Exception ex) { 
+                MessageBox.Show(ex.Message);
+            }
+            conn.Close();
+            RefreshControls();
+            var courses = GetAllCourses();
+            dtCourses.DataSource = courses;
+
+        }
+
+        private void LoadData_Click(object sender, EventArgs e)
+        {
+            var courses = GetAllCourses();
+            dtCourses.DataSource = courses;
+        }
+        List<Course> GetAllCourses()
+        {
+            string connString = @"Server=DESKTOP-VCKBA6J\SQLEXPRESS;Database=demo_m_sp21;Integrated Security=True;";
+            SqlConnection conn = new SqlConnection(connString);
+            List<Course> courses = new List<Course>();
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            string query = "select * from courses";
+            try
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Course c = new Course();
+                    c.Id = reader.GetInt32(reader.GetOrdinal("Id"));
+                    c.CourseName = reader.GetString(reader.GetOrdinal("CourseName"));
+                    c.CourseCode = reader.GetString(reader.GetOrdinal("CourseCode"));
+                    courses.Add(c);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            conn.Close();
+            return courses;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var courses = GetAllCourses();
+            dtCourses.DataSource = courses;
+        }
+        void RefreshControls()
+        {
+            tbCName.Text = "";
+            tbCCode.Text = "";
+        }
+    }
+}
